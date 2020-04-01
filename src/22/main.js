b1ca52ba61ac0453891b692a488a0cd4aff401a0
TFohrer <t.fohrer@gmx.de>
2020-04-01 23:25:50 +0200

    Challenge 22 - complete

---
+++ src/22/main.js
@@ -0,0 +1,25 @@
+(function(){
+
+    const highlight = document.querySelector('.highlight');
+    const links = document.querySelectorAll('a');
+
+    links.forEach(link => {
+        link.addEventListener('mouseover', updateHighlight);
+    })
+
+    function updateHighlight(event){
+        const linkPositions = this.getBoundingClientRect();
+        
+        const coords = {
+            width: linkPositions.width,
+            height: linkPositions.height,
+            top: linkPositions.top + window.scrollY,
+            left = linkPositions.left + window.scrollX
+        }
+
+        highlight.style.transform = `translateY(${coords.top}px) translateX(${coords.left}px)`;
+        highlight.style.width = `${coords.width}px`;
+        highlight.style.height = `${coords.height}px`;
+
+    }
+}())
